3f9cb35f0578691da51d4c3c5478c77d7996722c
Alex <Alexsamarkinru@gmail.com>
2019-10-26 17:10:14 +0300

    Add test2

---
+++ test2.py
@@ -0,0 +1,21 @@
+"""
+Author: Самаркин А.И.
+Date: 26/10/19
+Purpose: test
+"""
+
+# стандартная секция импорта, будет использоваться всегда
+# модули числовых расчетов
+import scipy as sc
+import numpy as np
+import pandas as pd
+# модуль библиотеки для вывода графиков в стиле Matlab
+import matplotlib
+
+matplotlib.use('Qt5Agg')  # библиотека для вывода графиков на базе Qt5, нужен импорт модуля PyQt5
+# модуль печати плоских графиков
+import matplotlib.pyplot as plt
+
+plt.style.use('bmh')  # можно попробовать также 'ggplot' или 'classic'
+fig = plt.figure()  # поверхность для вывода графиков (в виде всплывающего окна)
+
